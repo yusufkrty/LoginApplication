8a9672731966a7c235ca3af5434a48bc4e9f64ba
Ersin Alçin <ersin.alcin@venhancer.com>
2024-02-22 11:39:31 +0300

    fe: first test

---
+++ App.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login';
+import Home from './views/home';
+import PrivateRoute from './utils/privateRoute';
+
+function App() {
+
+
+  return (
+    <Router>
+      <Routes>
+
+        <Route path ="/home" element ={<PrivateRoute  Component={Home}  />} />
+        <Route path="/"  exact element={<Login />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
+
+
+
+
+
+
---
+++ components/login.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from "axios";
+
+import {useNavigate} from 'react-router-dom';
+import { validateForm } from '../utils/formValidation';
+import { setAuthToken } from "../utils/setAuthToken"
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [errors, setErrors] = useState("");
+    const navigate = useNavigate();
+
+    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const errors = validateForm(email, password)
+        
+        if (Object.keys(errors).length === 0) {
+            
+            try {
+                const response = await axios.post("http://localhost:3000/api/auth/login", {
+                    email,
+                    password,
+                    
+                    
+                });
+                const token = response.data.accessToken
+                localStorage.setItem("token",token);
+                setAuthToken(token);
+
+                const refreshToken = response.data.refreshToken;
+                localStorage.setItem("refreshToken", refreshToken);
+
+                setMessage(response.data.accessToken);
+                console.log("token "+response.data.accessToken);
+                navigate('/home');
+            } catch (error) {
+                console.error(error.response.data.message);
+                setErrors({login: error.response.data.message});
+            }
+            
+        }
+        else {
+            setErrors(errors);
+            console.log(errors);
+        }
+
+    };
+    return (
+        <div className='grid cols-1'>
+            <div className='flex flex-col justify-center items-center h-screen  bg-gray-100'>
+                <form onSubmit={handleSubmit} className='max-w-[400px] w-full  mx-auto bg-white p-4 border-1 shadow-lg'>
+                    <h2 className='text-4xl font-bold text-center my-6'>LOG IN</h2>
+                    <div className='flex flex-col py-2'>
+                        <label>E-mail</label>
+                        <input value={email} className='border p-2' type='text' onChange={(e) => setEmail(e.target.value)}></input>
+                        {errors.email && <p className="text-red-500">{errors.email}</p>}
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label>Password</label>
+                        <input value={password} className='border p-2' type='password' onChange={(e) => setPassword(e.target.value)}></input>
+                        {errors.password && <p className="text-red-500">{errors.password}</p>}
+                        {errors.login && <p className="text-red-500">{errors.login}</p>}
+                    </div>
+                    <button type='submit' className='border w-full my-5 p-2 bg-indigo-600  hover:bg-indigo-500 text-white'>Sign in</button>
+                    <p className='flex items-center'><input className='mr-2' type='checkbox'></input>Remember me</p>
+                </form>
+            </div>
+
+        </div>
+    )
+}
