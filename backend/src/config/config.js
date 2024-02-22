c847e5673ebcabd4a5e43ac31a61ccfb0ba1a94f
yusufkrty <96348130+yusufkrty@users.noreply.github.com>
2024-02-22 19:21:10 +0300

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     secretKey: 'venhancer',
-    mongoURI: 'mongodb://localhost:27017/node-login',
+    mongoURI: "mongodb+srv://resul1:Resul123.@cluster0.mbbk9a1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   };
